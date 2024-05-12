4d5cd6d820cd38950be40e01ae15abf2311672c8
kravshan <pasidub23@gmail.com>
2024-05-12 20:23:19 +0530

    IP config file

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,6 @@
 const googleMapsApiKey = 'AIzaSyC1uRusmT-peFFG8YKnmpJttYe_oN90p10';
 
-const deviceIp = '192.168.8.183';
+const deviceIp =
+    ''; //add your ip (Open cmd, type 'ipconfig' and get IPv4 ip address)
 
 const apiUrl = 'http://$deviceIp:3000/api/';
